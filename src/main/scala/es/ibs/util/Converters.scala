618df295abdfeb8e99c90139074b360f582bb929
Igor Kulatov <igor.kulatov@gmail.com>
2019-08-15 16:44:35 +0200

    HMAC - Converters

--- src/main/scala/es/ibs/util/Converters.scala
+++ src/main/scala/es/ibs/util/Converters.scala
@@ -4,18 +4,22 @@ import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
 import java.security.MessageDigest
 import java.util.Base64
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
 
 trait Converters {
+  import Converters._
 
   private val hexArray = "0123456789abcdef".toCharArray
 
   implicit class ByteArrayConverters(bytes: Array[Byte]) {
+    @inline final def md5: Array[Byte] = _digest(MD5, bytes)
+    @inline final def sha256: Array[Byte] = _digest(SHA256, bytes)
+    @inline final def sha512: Array[Byte] = _digest(SHA512, bytes)
 
-    @inline private def _digest(algo: String) = MessageDigest.getInstance(algo).digest(bytes)
-
-    @inline final def md5: Array[Byte] = _digest("MD5")
-    @inline final def sha256: Array[Byte] = _digest("SHA-256")
-    @inline final def sha512: Array[Byte] = _digest("SHA-512")
+    @inline final def hmac_md5(key: Array[Byte]): Array[Byte] = _mac(HMAC_MD5, key, bytes)
+    @inline final def hmac_sha256(key: Array[Byte]): Array[Byte] = _mac(HMAC_SHA256, key, bytes)
+    @inline final def hmac_sha512(key: Array[Byte]): Array[Byte] = _mac(HMAC_SHA512, key, bytes)
 
     @inline final def hex: String = {
       val hexChars = new Array[Char](bytes.length * 2)
@@ -32,16 +36,15 @@ trait Converters {
   }
 
   implicit class StringConverters(str: String) {
+    @inline final def getBytesUTF8: Array[Byte] = str.getBytes(StandardCharsets.UTF_8)
 
-    @inline private def _digest(algo: String) = MessageDigest.getInstance(algo).digest(str.getBytes(StandardCharsets.UTF_8))
+    @inline final def md5: Array[Byte] = _digest(MD5, str.getBytesUTF8)
+    @inline final def sha256: Array[Byte] = _digest(SHA256, str.getBytesUTF8)
+    @inline final def sha512: Array[Byte] = _digest(SHA512, str.getBytesUTF8)
 
-    @inline final def md5: Array[Byte] = _digest("MD5")
-    @inline final def sha256: Array[Byte] = _digest("SHA-256")
-    @inline final def sha512: Array[Byte] = _digest("SHA-512")
-
-    @inline final def md5_hex: String = md5.hex
-    @inline final def sha256_hex: String = sha256.hex
-    @inline final def sha512_hex: String = sha512.hex
+    @inline final def hmac_md5(key: Array[Byte]): Array[Byte] = _mac(HMAC_MD5, key, str.getBytesUTF8)
+    @inline final def hmac_sha256(key: Array[Byte]): Array[Byte] = _mac(HMAC_SHA256, key, str.getBytesUTF8)
+    @inline final def hmac_sha512(key: Array[Byte]): Array[Byte] = _mac(HMAC_SHA512, key, str.getBytesUTF8)
 
     @inline final def urlEncode: String = URLEncoder.encode(str, StandardCharsets.UTF_8)
 
@@ -57,5 +60,22 @@ trait Converters {
 
   implicit class RichOptional[T](opt: java.util.Optional[T]) {
     @inline def asScala: Option[T] = if(opt.isPresent) Some(opt.get()) else None
+  }
+}
+
+private object Converters {
+  private val MD5 = "MD5"
+  private val SHA256 = "SHA-256"
+  private val SHA512 = "SHA-512"
+
+  private val HMAC_MD5 = "HmacMD5"
+  private val HMAC_SHA256 = "HmacSHA256"
+  private val HMAC_SHA512 = "HmacSHA512"
+
+  @inline private def _digest(algo: String, bytes: Array[Byte]) = MessageDigest.getInstance(algo).digest(bytes)
+
+  @inline private def _mac(algo: String, key: Array[Byte], bytes: Array[Byte]) = pro(Mac.getInstance(algo)) { mac =>
+    mac.init(new SecretKeySpec(key, algo))
+    mac.doFinal(bytes)
   }
 }
